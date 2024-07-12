415776bfe03c074b13eb6ad3d90363e637730b90
Manasa-Gp <manasa.g.pavushetty@gmail.com>
2024-07-12 17:02:44 -0400

    first commit

---
+++ Lab5/WorkingWithObjects.js
@@ -0,0 +1,5 @@
+app.get("/lab5/assignment/title/:newTitle", (req, res) => {
+    const { newTitle } = req.params;
+    assignment.title = newTitle;
+    res.json(assignment);
+  });
---
+++ Lab5/index.js
@@ -0,0 +1,15 @@
+import PathParameters from "./PathParameters.js";
+export default function Lab5(app) {
+  app.get("/lab5/welcome", (req, res) => {
+    res.send("Welcome to Lab 5");
+  });
+  PathParameters(app);
+}
+export default function WorkingWithObjects(app) {
+  app.get("/lab5/assignment", (req, res) => {
+    res.json(assignment);
+  });
+  app.get("/lab5/assignment/title", (req, res) => {
+    res.json(assignment.title);
+  });
+}
