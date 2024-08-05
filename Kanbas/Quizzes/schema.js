361735a8d7925877883bc3d122dd4c81f04f0ba2
Manasa-Gp <manasa.g.pavushetty@gmail.com>
2024-08-05 19:51:05 -0400

    commit

--- Kanbas/Quizzes/schema.js
+++ Kanbas/Quizzes/schema.js
@@ -1,5 +1,17 @@
 import mongoose from "mongoose";
 
+const QuestionSchema = new mongoose.Schema({
+    type:String,
+    question:String,
+    options: {
+        type: Map,
+        of: String
+    },
+    answer:String,
+    quiz: String,
+    points:Number
+  });
+
 const QuizSchema = new mongoose.Schema({
     title: String,
     description: String, 
@@ -21,6 +33,7 @@ const QuizSchema = new mongoose.Schema({
     oneQuestionAtATime: Boolean,
     webcamRequired: Boolean,
     lockQuestionsAfterAnswering: Boolean,
+    questions:[QuestionSchema]
 }, { collection: "quizzes" });
 
 export default QuizSchema;
